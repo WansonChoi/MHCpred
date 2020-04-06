1f953b205fc0cde86a2a6d419ddcc766947ea072
WansonChoi <wschoi.bhlab@gmail.com>
2020-04-06 14:42:37 +0900

    (ldpred) prepared modified version of ldpred statically.

---
+++ ldpred/__init__.py
@@ -0,0 +1,21 @@
+"""
+LDpred
+"""
+
+__version__ = '1.0.11'
+__update_date__ = '20 November 2019'
+__author__ = 'Bjarni J Vilhjalmsson'
+__contact__ = 'email: bjarni.vilhjalmsson@gmail.com'
+__url__ = 'https://github.com/bvilhjal/ldpred'
+
+
+
+"""
+MHCpred
+
+MHCpred is created based on LDpred. 
+The modified version of LDpred is prepared statically in MHCpred project.
+ 
+"""
+__author_MHCpred__ = 'WansonChoi'
+__contact_MHCpred__ = 'wansonchoi@snu.ac.kr'
---
+++ ldpred/__main__.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/env python -m
+
+from ldpred import run_WS
+
+    
+
+if __name__ == '__main__':
+    run_WS.main()
